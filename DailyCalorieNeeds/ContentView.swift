83c3a08fbd7ae656b4cd83a2238cdcb44edadc56
Ethan Chen <echen@lcs.on.ca>
2025-01-20 09:36:56 -0500

    Initial Commit

---
+++ DailyCalorieNeeds/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyCalorieNeeds
+//
+//  Created by Pak Ching Ethan Chen on 2025-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyCalorieNeeds/DailyCalorieNeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCalorieNeedsApp.swift
+//  DailyCalorieNeeds
+//
+//  Created by Pak Ching Ethan Chen on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct DailyCalorieNeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
