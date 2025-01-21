d8efa3b90d51109f76bf5becbfa9957d684ca313
Ethan Chen <echen@lcs.on.ca>
2025-01-21 11:30:30 -0500

    added func to check weight input and made button, also made feedback var

--- DailyCalorieNeeds/Views/DailyCaloriesCalculator.swift
+++ DailyCalorieNeeds/Views/DailyCaloriesCalculator.swift
@@ -13,6 +13,7 @@ struct DailyCaloriesCalculator: View {
     @State var height = ""
     @State var age = ""
     @State var PAL: Double = 1.2
+    @State var feedback = ""
     
     //MARK: Computed Properties
     
@@ -29,10 +30,10 @@ struct DailyCaloriesCalculator: View {
             TextField("Input Weight (kg) [eg. 82.1]", text: $weight)
                 .font(.system(size:20))
             
-            TextField("Input Height (m) [eg. 1.78]", text: $weight)
+            TextField("Input Height (m) [eg. 1.78]", text: $height)
                 .font(.system(size:20))
             
-            TextField("Input Age (yrs) [eg. 20]" , text: $weight)
+            TextField("Input Age (yrs) [eg. 20]" , text: $age)
                 .font(.system(size:20))
             
             //Slider and Label
@@ -41,12 +42,31 @@ struct DailyCaloriesCalculator: View {
                 
                 Text("Physical Activity Level: \(PAL.formatted(.number.precision(.fractionLength(1))))")
             }
+            Button{
+                checkInputs()
+            } label:{
+                Text("Calculate")
+            }
+            .buttonStyle(.borderedProminent)
+            Text("\(feedback)")
             Spacer()
         }
         .padding()
+        
+    }
+    //MARK: Functions
+    func checkInputs(){
+        
+        guard let weightValue = Double(weight)
+        else {
+            feedback += "Please input valid values"
+            return
+        }
     }
 }
 
 #Preview {
     DailyCaloriesCalculator()
 }
+
+
